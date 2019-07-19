231c31154a642e9e34c9c9c7c066ac2da4ff14b8
Alex <bilyksaha01@gmail.com>
2019-07-19 03:02:30 +0300

    completed the desktop version of the page

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,36 @@ import $ from 'jquery'
 window.jQuery = $;
 window.$ = $;
 
-import 'slick-carousel/slick/slick';
+import 'jquery-ui-slider/jquery-ui.min';
+import Swiper from 'swiper/dist/js/swiper.min';
+
+// Settings for sliders
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    navigation: {
+        prevEl: '.main__info__slider__arrow-prev',
+        nextEl: '.main__info__slider__arrow-next'
+    }
+});
 
 $(document).ready(function() {
+    // Inputs range
 
-    // Settings for sliders
+    $('.main__info__form .form').find('.range_default').each(function (index, item) {
+       $(item).slider({
+           range: "min",
+           value: 40,
+           orientation: "horizontal",
+           min: 0,
+           max: 100,
+           step: 1,
+           animate: true
+       });
+       $(item).attr('name', index);
+    });
+
     // Ban on entering letters
 
     const inputsPhone = document.querySelectorAll('.inputPhone-js');
